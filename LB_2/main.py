79d2d5933eaedbac8bba170be62f2119bb2ec434
tsu-khan-yuk <vania8282125@gmail.com>
2020-10-19 15:04:44 +0300

    added local

--- LB_2/main.py
+++ LB_2/main.py
@@ -3,6 +3,7 @@
 # Забезпечити реалізацію пошуку за декількома атрибутами з двох та більше сутностей одночасно: для числових атрибутів – у рамках діапазону, для рядкових – як шаблон функції LIKE оператора SELECT SQL, для логічного типу – значення True/False, для дат – у рамках діапазону дат.
 # Програмний код виконати згідно шаблону MVC (модель-подання-контролер)
 
+from .local import db_settings
 import datetime as date 
 import psycopg2
 
@@ -14,11 +15,11 @@ if __name__ == '__main__':
     print('PyPSQL console 0.9(Based on Python 3.6.9+) [{}]'.format(date.datetime.now()))
     try:
         connection = psycopg2.connect(
-            database="lab_1", 
-            user="ivan",
-            password="0000", 
-            host="localhost", 
-            port=5432
+            database=db_settings['database'], 
+            user=db_settings['user'],
+            password=db_settings['password'], 
+            host=db_settings['host'], 
+            port=db_settings['port']
         )
         flag = True
     except Exception as ex:
